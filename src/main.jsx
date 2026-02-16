f05bba02093fd3f5db6b27ebecc6ae6cd9583316
Lachlan Wallbridge <lachlanwallbridge@icloud.com>
2026-02-16 12:56:05 +1100

    Added vercel Analytics package

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
-import { Analytics } from "@vercel/analytics/next";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Analytics />
+    <>
+      <App />
+      <Analytics />
+    </>
   </StrictMode>,
 );
